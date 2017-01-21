54ef187c0c0ddf5772db584382ec10d009b102c9
osinga <osinga@users.noreply.github.com>
2017-01-21 13:29:20 +0100

    Use camelCase for AppleScript variables

--- .tmux/current_track.sh
+++ .tmux/current_track.sh
@@ -2,32 +2,32 @@
 # Source: https://www.tylerewing.co/tmux-now-playing
 
 CURRENT_TRACK=$(osascript <<EOF
-set spotify_state to false
-set itunes_state to false
+set spotifyState to false
+set iTunesState to false
 
-if is_app_running("Spotify") then
-    tell application "Spotify" to set spotify_state to (player state as text)
-else if is_app_running("iTunes") then
-    tell application "iTunes" to set itunes_state to (player state as text)
+if appIsRunning("Spotify") then
+    tell application "Spotify" to set spotifyState to (player state as text)
+else if appIsRunning("iTunes") then
+    tell application "iTunes" to set iTunesState to (player state as text)
 end if
 
-if spotify_state is equal to "playing" then
+if spotifyState is equal to "playing" then
     tell application "Spotify"
-        set track_name to name of current track
-        set artist_name to artist of current track
-        return artist_name & " - " & track_name & " | "
+        set trackName to name of current track
+        set artistName to artist of current track
+        return artistName & " - " & trackName & " | "
     end tell
-else if itunes_state is equal to "playing" then
+else if iTunesState is equal to "playing" then
     tell application "iTunes"
-        set track_name to name of current track
-        set artist_name to artist of current track
-        return artist_name & " - " & track_name & " | "
+        set trackName to name of current track
+        set artistName to artist of current track
+        return artistName & " - " & trackName & " | "
     end tell
 end if
 
-on is_app_running(app_name)
-    tell application "System Events" to (name of processes) contains app_name
-end is_app_running
+on appIsRunning(appName)
+    tell application "System Events" to (name of processes) contains appName
+end appIsRunning
 
 EOF)
 
