a24eea7aa3954ce41d95964795e2245d4c6258af
osinga <osinga@users.noreply.github.com>
2017-03-18 17:28:42 +0100

    Add an install script for global npm packages

--- install/install.sh
+++ install/install.sh
@@ -6,3 +6,4 @@ xcode-select --install              # Install the Xcode command line tools
 
 # Install
 source brew.sh                      # Install Homebrew and related components
+source node.sh                      # Update npm and install global packages
---
+++ install/node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+################################################################################
+# npm                                                                          #
+################################################################################
+
+# Update
+npm install npm@latest -g
+
+# Install
+npm install -g feathers-cli         # Feathers CLI
+npm install -g vue-cli              # Vue CLI
