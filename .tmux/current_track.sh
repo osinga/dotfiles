aa5ccb0dae1e6d57246ca0612c2a170c57d08707
osinga <osinga@users.noreply.github.com>
2017-03-23 18:32:29 +0100

    Tweak tmux status bar design

--- .tmux/current_track.sh
+++ .tmux/current_track.sh
@@ -15,13 +15,13 @@ if spotifyState is equal to "playing" then
     tell application "Spotify"
         set trackName to name of current track
         set artistName to artist of current track
-        return artistName & " - " & trackName & " | "
+        return artistName & " - " & trackName & " · "
     end tell
 else if iTunesState is equal to "playing" then
     tell application "iTunes"
         set trackName to name of current track
         set artistName to artist of current track
-        return artistName & " - " & trackName & " | "
+        return artistName & " - " & trackName & " · "
     end tell
 end if
 
