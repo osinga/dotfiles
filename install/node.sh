98b2e78deccef87bdfa108a11a74406c7bbc708b
osinga <osinga@users.noreply.github.com>
2017-04-07 22:41:04 +0200

    Add a global update script

--- install/node.sh
+++ install/node.sh
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Update
-npm install npm@latest -g
+npm install npm@latest -g           # Update to the latest version of npm
 
 # Install
 npm install -g feathers-cli         # Feathers CLI
---
+++ install/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+################################################################################
+# Update                                                                       #
+################################################################################
+
+# App Store
+sudo softwareupdate -i -a           # Update App Store applications
+
+# Homebrew
+brew update                         # Update to the latest version of Homebrew
+brew upgrade                        # Upgrade any already-installed formulae
+brew cleanup                        # Remove cached downloads and old versions
+
+# Node
+npm install npm@latest -g           # Update to the latest version of npm
