62e2e09a2dd58bc1b65130ab93908c327267d1cc
osinga <osinga@users.noreply.github.com>
2017-04-08 17:42:09 +0200

    Add nodemon to the node install script

--- install/node.sh
+++ install/node.sh
@@ -9,4 +9,5 @@ npm install npm@latest -g           # Update to the latest version of npm
 
 # Install
 npm install -g feathers-cli         # Feathers CLI
+npm install -g nodemon              # Nodemon
 npm install -g vue-cli              # Vue CLI
