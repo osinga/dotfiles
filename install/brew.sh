306873b91bab8b4d568e13ac1d6919de59890beb
osinga <osinga@users.noreply.github.com>
2017-06-23 09:54:55 +0200

    Install Tig

--- install/brew.sh
+++ install/brew.sh
@@ -33,6 +33,7 @@ brew install node                   # JavaScript runtime environment
 brew install php71                  # PHP 7.1
 brew install python3                # Python 3
 brew install reattach-to-user-namespace         # macOS pasteboard access in tmux
+brew install tig                    # Text-mode interface for git
 brew install tmux                   # Terminal multiplexer
 brew install tree                   # Recursive directory listing
 brew install wget                   # Download Web files
