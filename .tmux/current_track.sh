b2ebfad6f01e3eb8cc9d2dd499eb02587a152dbf
osinga <osinga@users.noreply.github.com>
2018-08-11 14:30:23 +0200

    Remove iTunes from the current_track script

--- .tmux/current_track.sh
+++ .tmux/current_track.sh
@@ -3,22 +3,13 @@
 
 CURRENT_TRACK=$(osascript <<EOF
 set spotifyState to false
-set iTunesState to false
 
 if appIsRunning("Spotify") then
     tell application "Spotify" to set spotifyState to (player state as text)
-else if appIsRunning("iTunes") then
-    tell application "iTunes" to set iTunesState to (player state as text)
 end if
 
 if spotifyState is equal to "playing" then
     tell application "Spotify"
-        set trackName to name of current track
-        set artistName to artist of current track
-        return artistName & " - " & trackName & " · "
-    end tell
-else if iTunesState is equal to "playing" then
-    tell application "iTunes"
         set trackName to name of current track
         set artistName to artist of current track
         return artistName & " - " & trackName & " · "
