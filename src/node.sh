3a8a4a86580135f5202bdf46fa85226279cab519
osinga <osinga@users.noreply.github.com>
2021-02-17 17:04:23 +0100

    Clean up Node setup

--- src/node.sh
+++ src/node.sh
@@ -6,7 +6,6 @@
 npm install -g npm@latest           # Update to the latest version of npm
 
 # Install
-npm install -g @feathersjs/cli      # Feathers CLI
 npm install -g eslint               # ESLint
 npm install -g faker-cli            # Faker
 npm install -g typescript           # TypeScript
