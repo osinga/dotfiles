e564085312e1ee400ab795f8678a4f4dc7f58ff4
osinga <osinga@users.noreply.github.com>
2021-02-17 22:42:20 +0100

    Tweak the tmux status line

--- .tmux/current_track.sh
+++ .tmux/current_track.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_TRACK=$(osascript <<EOF
+osascript <<EOD
 set SpotifyState to false
 
 if AppIsRunning("Spotify") then
@@ -11,14 +11,11 @@ if SpotifyState is equal to "playing" then
     tell application "Spotify"
         set TrackName to name of current track
         set ArtistName to artist of current track
-        return "♫ " & ArtistName & " - " & TrackName & " · "
+        return "♫ " & ArtistName & " - " & TrackName
     end tell
 end if
 
 on AppIsRunning(AppName)
     tell application "System Events" to (name of processes) contains AppName
 end AppIsRunning
-
-EOF)
-
-echo $CURRENT_TRACK
+EOD
