4ac7253635d1fc10fa0bea77868c411abafb8687
osinga <osinga@users.noreply.github.com>
2021-02-17 16:52:26 +0100

    Clean up and update Homebrew setup

--- src/brew.sh
+++ src/brew.sh
@@ -3,11 +3,7 @@
 ################################################################################
 
 # Install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-# Update
-brew update                         # Update to the latest version of Homebrew
-brew upgrade                        # Upgrade any already-installed formulae
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
 
@@ -29,11 +25,7 @@ brew install the_silver_searcher    # Search through source code
 brew install tldr                   # Simplified man pages
 brew install tmux                   # Terminal multiplexer
 brew install vim                    # Use up-to-date vim
-brew install zsh                    # Alternative shell
 brew install zsh-completions        # Additional Zsh completions
-
-# Cleanup
-brew cleanup                        # Remove cached downloads and old versions
 
 
 
@@ -41,32 +33,28 @@ brew cleanup                        # Remove cached downloads and old versions
 # Applications                                                                 #
 ################################################################################
 
-# Setup
-brew tap homebrew/cask              # Tap Homebrew Cask
-
 # Install
-brew cask install 1password         # 1Password
-brew cask install alfred            # Alfred
-brew cask install calibre           # Calibre
-brew cask install firefox           # Firefox
-brew cask install google-chrome     # Google Chrome
-brew cask install imageoptim        # ImageOptim
-brew cask install insomnia          # Insomnia
-brew cask install karabiner-elements    # Karabiner Elements
-brew cask install keyboard-maestro  # Keyboard Maestro
-brew cask install notion            # Notion
-brew cask install obsidian          # Obsidian
-brew cask install pock              # Pock
-brew cask install sketch            # Sketch
-brew cask install slack             # Slack
-brew cask install soda-player       # Soda Player
-brew cask install spotify           # Spotify
-brew cask install tableplus         # TablePlus
-brew cask install telegram          # Telegram
-brew cask install tor-browser       # Tor Browser
-brew cask install transmission      # Transmission
-brew cask install visual-studio-code    # Visual Studio Code
-brew cask install whatsapp          # WhatsApp
+brew install 1password              # 1Password
+brew install adguard                # AdGuard
+brew install alfred                 # Alfred
+brew install calibre                # Calibre
+brew install firefox                # Firefox
+brew install google-chrome          # Google Chrome
+brew install imageoptim             # ImageOptim
+brew install insomnia               # Insomnia
+brew install karabiner-elements     # Karabiner Elements
+brew install keyboard-maestro       # Keyboard Maestro
+brew install notion                 # Notion
+brew install pock                   # Pock
+brew install slack                  # Slack
+brew install soda-player            # Soda Player
+brew install spotify                # Spotify
+brew install tableplus              # TablePlus
+brew install tor-browser            # Tor Browser
+brew install transmission           # Transmission
+brew install visual-studio-code     # Visual Studio Code
+brew install whatsapp               # WhatsApp
+brew install zotero                 # Zotero
 
 
 
@@ -78,20 +66,14 @@ brew cask install whatsapp          # WhatsApp
 brew tap homebrew/cask-fonts        # Tap Homebrew Fonts
 
 # Install
-brew cask install font-iosevka      # Iosevka
-brew cask install font-open-sans    # Open Sans
-
-# Cleanup
-brew cask cleanup                   # Remove cached downloads and tracker symlinks
+brew install font-iosevka           # Iosevka
+brew install font-open-sans         # Open Sans
 
 
 
 ################################################################################
 # Services                                                                     #
 ################################################################################
-
-# Setup
-brew tap homebrew/services          # Tap Homebrew Services
 
 # Start
 brew services start mysql           # MySQL
